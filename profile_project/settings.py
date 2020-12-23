2fe940633eeca5808b6f6822ae058d7923c33185
Bondith <kolbondith@gmail.com>
2020-12-23 22:14:33 +0700

    Configure Deploy API

--- profile_project/settings.py
+++ profile_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '6oh(krm-8&huzj67^=s^s2^fk33x_0&*#2$ye*2zil0%ser_*3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEGUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profile_api.UserProfile'
+
+STATIC_ROOT = 'static/'
