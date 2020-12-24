0f7d25550684b91f177bd8768924160487b62258
Bondith <kolbondith@gmail.com>
2020-12-24 11:03:22 +0700

    Added Allow Host

--- profile_project/settings.py
+++ profile_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '6oh(krm-8&huzj67^=s^s2^fk33x_0&*#2$ye*2zil0%ser_*3'
 DEBUG = bool(int(os.environ.get('DEGUG', 1)))
 
 ALLOWED_HOSTS = [
-    'http://ec2-3-135-223-33.us-east-2.compute.amazonaws.com/',
+    'ec2-3-17-63-79.us-east-2.compute.amazonaws.com',
     '127.0.0.1'
     ]
 
